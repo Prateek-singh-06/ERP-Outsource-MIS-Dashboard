768a1eeaef1cb4410d749f402afd3b56c6292b0e
Prateek Singh <prateek99555@gmail.com>
2025-07-12 12:40:27 +0530

    rego financials completed

--- src/lib/googleSheets.ts
+++ src/lib/googleSheets.ts
@@ -6,6 +6,7 @@ import {
   Plant,
   Rego,
   RegoBarData,
+  RegoFinancialData,
   RegoPieData,
   RegoSummary,
   Safety,
@@ -174,16 +175,16 @@ export async function fetchGoogleSheetDataSafteyAttendance(
       totalWorkdays: rawData[19]["_38"],
       totalLeaves: rawData[19]["_43"],
       totalPresents: rawData[19]["_45"],
-      paidLeaves:rawData[19]["_44"],
-      weeklyOff:rawData[19]["_40"],
-      Holidays:rawData[19]["_39"]
+      paidLeaves: rawData[19]["_44"],
+      weeklyOff: rawData[19]["_40"],
+      Holidays: rawData[19]["_39"],
     };
     console.log(summary);
     const employees = [];
     for (let i = 3; i < 19; i++) {
       const row = rawData[i];
       if (!row[""]) {
-        summary.totalEmployees=i-3;
+        summary.totalEmployees = i - 3;
         continue;
       }
       const attendance = [];
@@ -268,23 +269,20 @@ export async function fetchGoogleSheetDataRego(
   }
 
   const regosummary: RegoSummary = {
-    TotalKM: typedData[Math.min(35, totalLength - 1)]["Total Running Kms as per REGO"],
+    TotalKM:
+      typedData[Math.min(35, totalLength - 1)]["Total Running Kms as per REGO"],
     TotalExtraKM: typedData[Math.min(35, totalLength - 1)]["EXTRA KMS REGO"],
     TotalExtraHour: extractNumber(
       String(typedData[Math.min(35, totalLength - 1)]["Billed Extra Hours"])
     ),
     TotalExtraBill: extractNumber(
-      String(
-        typedData[Math.min(35, totalLength - 1)][
-          "Bill By Rego"
-        ]
-      )
+      String(typedData[Math.min(35, totalLength - 1)]["Bill By Rego"])
     ),
-    AverageKMPerVehiclePerDay:0,
-    AverageExtraKMPerVehiclePerDay:0,
-    AverageExtraHourPerVehiclePerDay:0,
-    AverageBillPerVehicalPerDay:0,
-    TotalDays:typedData[Math.min(35, totalLength - 1)]["Running days"]
+    AverageKMPerVehiclePerDay: 0,
+    AverageExtraKMPerVehiclePerDay: 0,
+    AverageExtraHourPerVehiclePerDay: 0,
+    AverageBillPerVehicalPerDay: 0,
+    TotalDays: typedData[Math.min(35, totalLength - 1)]["Running days"],
   };
   if (type !== "all") {
     regosummary.TotalKM = 0;
@@ -292,28 +290,30 @@ export async function fetchGoogleSheetDataRego(
     regosummary.TotalExtraHour = 0;
     regosummary.TotalExtraBill = 0;
     // totalSelectedVehicleLength=0
-    regosummary.TotalDays=0;
+    regosummary.TotalDays = 0;
     for (let i = 0; i < Math.min(35, totalLength - 1); i++) {
       const row = typedData[i];
-      
+
       if (String(row["Type"]) === type) {
         // totalSelectedVehicleLength++;
         regosummary.TotalKM += row["Total Running Kms as per REGO"] || 0;
         regosummary.TotalExtraBill +=
-          extractNumber(
-            String(row["Bill By Rego"])
-          ) || 0;
+          extractNumber(String(row["Bill By Rego"])) || 0;
         regosummary.TotalExtraKM += row["EXTRA KMS REGO"] || 0;
         regosummary.TotalExtraHour +=
-        extractNumber(String(row["Billed Extra Hours"])) || 0;
-        regosummary.TotalDays+=row["Running days"]||0;
+          extractNumber(String(row["Billed Extra Hours"])) || 0;
+        regosummary.TotalDays += row["Running days"] || 0;
       }
     }
   }
-  regosummary.AverageKMPerVehiclePerDay = regosummary.TotalKM  / (regosummary.TotalDays ?? 1);
-  regosummary.AverageExtraKMPerVehiclePerDay=regosummary.TotalExtraKM / (regosummary.TotalDays ?? 1);
-  regosummary.AverageExtraHourPerVehiclePerDay=regosummary.TotalExtraHour/(regosummary.TotalDays ?? 1);
-  regosummary.AverageBillPerVehicalPerDay=regosummary.TotalExtraBill/(regosummary.TotalDays ?? 1);
+  regosummary.AverageKMPerVehiclePerDay =
+    regosummary.TotalKM / (regosummary.TotalDays ?? 1);
+  regosummary.AverageExtraKMPerVehiclePerDay =
+    regosummary.TotalExtraKM / (regosummary.TotalDays ?? 1);
+  regosummary.AverageExtraHourPerVehiclePerDay =
+    regosummary.TotalExtraHour / (regosummary.TotalDays ?? 1);
+  regosummary.AverageBillPerVehicalPerDay =
+    regosummary.TotalExtraBill / (regosummary.TotalDays ?? 1);
 
   const RegoPieData: RegoPieData[] = [];
   const BaseRent = {
@@ -328,14 +328,14 @@ export async function fetchGoogleSheetDataRego(
     name: "Extra hour Charges",
     value: 0,
   };
-  const UtilizationData=[];
+  const UtilizationData = [];
   for (let i = 0; i < Math.min(35, totalLength - 1); i++) {
     const row = typedData[i];
     if (type === "all" || String(row["Type"]) === type) {
-      const VehicleUtilization={
-          Utilization:extractNumber(String(row["UTILIZATION % "])),
-          Vehicle:String(row["Vehicle No"])
-      }
+      const VehicleUtilization = {
+        Utilization: extractNumber(String(row["UTILIZATION % "])),
+        Vehicle: String(row["Vehicle No"]),
+      };
       UtilizationData.push(VehicleUtilization);
       BaseRent.value += extractNumber(String(row["Actual Rent"])) || 0;
       ExtraKMCharges.value +=
@@ -378,7 +378,129 @@ export async function fetchGoogleSheetDataRego(
     Summary: regosummary,
     PieData: RegoPieData,
     BarData: RegoBarData,
-    Utilization:UtilizationData,
+    Utilization: UtilizationData,
   };
   return Rego;
 }
+// export async function fetchGoogleSheetDataRegoFinancials(
+//   gid: string | null,
+//   type: string | null,
+//   Month?: string | null
+// ): Promise<RegoFinancialData[]> {
+//   if (!Month) {
+//     Month = new Date()
+//       .toLocaleString("default", { month: "long" })
+//       .toUpperCase();
+//   }
+//   const regodatafiltered = regodata.filter((row) => row.month === Month);
+//   if (regodatafiltered.length === 0) {
+//     throw new Error(`No data found for month: ${Month}`);
+//   }
+//   // https://docs.google.com/spreadsheets/d/e/2PACX-1vQSzrdrjtBTctvX7VLe24y2ZNWEHszbU6Ndv8ZnKmX2x7D9IPHLAltwfyfpUxZMw3WkyeRTk5rxLOVY/pubhtml?gid=360819154&single=true
+//   const REGO_FINANCIALS_CSV_URL = `https://docs.google.com/spreadsheets/d/e/2PACX-1vQSzrdrjtBTctvX7VLe24y2ZNWEHszbU6Ndv8ZnKmX2x7D9IPHLAltwfyfpUxZMw3WkyeRTk5rxLOVY/pub?gid=360819154&single=true&output=csv`;
+//   const response2 = await fetch(REGO_FINANCIALS_CSV_URL, {
+//     next: { revalidate: 0 },
+//   });
+
+//   const csvData = await response2.text();
+
+//   const { data } = Papa.parse(csvData, {
+//     header: true,
+//     skipEmptyLines: true,
+//     dynamicTyping: true,
+//   });
+
+//   // Assert type for data rows
+//   // const typedData = data as Array<Record<string, number>>;
+//   const typedData = data as Array<Record<string, string | number>>;
+//   // console.log(typedData);
+
+//   const totalLength = typedData.length;
+
+//   function extractNumber(value: string | undefined): number {
+//     if (!value) return 0;
+//     // Remove all non-digit and non-decimal characters
+//     const num = value.replace(/[^0-9.]/g, "");
+//     return Number(num) || 0;
+//   }
+//   const FinancialsData = [];
+//   for (let i = 0; i < totalLength; i++) {
+//     const row = typedData[i];
+//     if (row["Type"] === "ETS") {
+//       const Financials = {
+//         month: String(row["MONTH"] ?? ""),
+//         vendorClaimed: row["Rego Bill"] ?? 0,
+//         calculated: row["RML Bill"] ?? 0,
+//         deductions: row["Deduction"] ?? 0,
+//         hold: row["On hold"] ?? 0,
+//         paid: row["RML Approved"] ?? 0,
+//         status: "Paid",
+//       };
+
+//       FinancialsData.push(Financials);
+//     }
+//   }
+//   return FinancialsData;
+// }
+export async function fetchGoogleSheetDataRegoFinancials(
+  gid: string | null,
+  type: string | null,
+  Month?: string | null
+): Promise<RegoFinancialData[]> {
+  if (!Month) {
+    Month = new Date()
+      .toLocaleString("default", { month: "long" })
+      .toUpperCase();
+  }
+
+  const regodatafiltered = regodata.filter((row) => row.month === Month);
+  if (regodatafiltered.length === 0) {
+    throw new Error(`No data found for month: ${Month}`);
+  }
+
+  const REGO_FINANCIALS_CSV_URL = `https://docs.google.com/spreadsheets/d/e/2PACX-1vQSzrdrjtBTctvX7VLe24y2ZNWEHszbU6Ndv8ZnKmX2x7D9IPHLAltwfyfpUxZMw3WkyeRTk5rxLOVY/pub?gid=360819154&single=true&output=csv`;
+  const response2 = await fetch(REGO_FINANCIALS_CSV_URL, {
+    next: { revalidate: 0 },
+  });
+
+  const csvData = await response2.text();
+
+  const { data } = Papa.parse(csvData, {
+    header: true,
+    skipEmptyLines: true,
+    dynamicTyping: true,
+  });
+
+  const typedData = data as Array<Record<string, string | number>>;
+  const totalLength = typedData.length;
+
+  function extractNumber(value: string | number | undefined): number {
+    if (value === undefined || value === null) return 0;
+    if (typeof value === "number") return value;
+    const num = value.replace(/[^0-9.]/g, "");
+    return Number(num) || 0;
+  }
+
+  const FinancialsData: RegoFinancialData[] = [];
+
+  for (let i = 0; i < totalLength; i++) {
+    const row = typedData[i];
+
+    if (row["Type"] === "ETS") {
+      const Financials: RegoFinancialData = {
+        month: String(row["MONTH"] ?? ""),
+        vendorClaimed: extractNumber(row["Rego Bill"]),
+        calculated: extractNumber(row["RML Bill"]),
+        deductions: extractNumber(row["Deduction"]),
+        hold: extractNumber(row["On hold"]),
+        paid: extractNumber(row["RML Approved"]),
+        status: "Paid",
+      };
+
+      FinancialsData.push(Financials);
+    }
+  }
+
+  return FinancialsData;
+}
+
